611610624f0117351b99ea56cc311c08877700c9
Ruslan <phedotov-rus@yandex.ru>
2016-06-05 13:43:05 +0700

    Finish static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Welcome | Real-time chat application"
+    assert_select "h1", "Welcome to our site!"
   end
 
 end
