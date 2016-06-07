d047a57e1ec35e8319b997426301bace54acbf56
Ruslan <phedotov-rus@yandex.ru>
2016-06-07 15:15:01 +0700

    Add a tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,5 +6,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'pages/index'
     assert_select "a[href=?]", root_path
+    assert_select "title", full_title("Welcome")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
