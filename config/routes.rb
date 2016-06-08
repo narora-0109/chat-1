d8282e94d79ad4b1d66bfcd34f1bbbcf4b19a28b
Ruslan <phedotov-rus@yandex.ru>
2016-06-08 12:42:58 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#index'
 
+  root 'pages#index'
   get 'signup' => 'users#new'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
