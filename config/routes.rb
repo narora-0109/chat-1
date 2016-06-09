caf4f3fb478ed185de76eeeec7c9bd41ecbafa63
Ruslan <phedotov-rus@yandex.ru>
2016-06-09 16:37:56 +0700

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'pages#index'
-  get 'signup' => 'users#new'
+  root                'pages#index'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
