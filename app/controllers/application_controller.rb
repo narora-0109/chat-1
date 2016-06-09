eb2cc3ffac44f7d50f4855717c9361f6dc4f5e21
Ruslan <phedotov-rus@yandex.ru>
2016-06-10 00:52:36 +0700

    Fix errors with access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
   include SessionsHelper
+  skip_before_action :require_login
+
+  private
+
+    def require_login
+      if logged_in?
+        redirect_to root_url
+      end
+    end
 end
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :require_login, only: [:new]
+
   def new
   end
 
